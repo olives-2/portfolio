8cf04b5d568aaecaed1156a33d186a28d8b69fa6
Matis Olives <matisolives@gmail.com>
2024-11-17 18:07:27 +0100

    Updates

---
+++ _app/immutable/entry/start.jOG8c615.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Kr0sdHuu.js";export{t as start};
--- _app/immutable/nodes/3.BfNmOVng.js
+++
@@ -1,1 +0,0 @@
-import{a as p,t}from"../chunks/disclose-version.CDupqakr.js";import"../chunks/legacy.xk-FiD-J.js";var e=t("<h1>Cape</h1>");function n(a){var o=e();p(a,o)}export{n as component};
